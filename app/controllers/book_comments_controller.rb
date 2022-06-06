1e983775791a47e46bd91fa9c3337c8c110dcf52
neko385 <102905708+neko385@users.noreply.github.com>
2022-06-06 04:16:46 +0000

    [Fix]コメント機能

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -8,7 +8,7 @@ class BookCommentsController < ApplicationController
   end
 
   def destroy
-    BookComment.find(params[:id]).destroy
+    BookComment.find(params[:book_id]).destroy
     redirect_to request.referer
   end
 
