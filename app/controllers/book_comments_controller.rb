1fdd6e60f3521dec0734152555035a877b847697
neko385 <102905708+neko385@users.noreply.github.com>
2022-06-07 14:39:01 +0000

    [Add]コメント機能非同期通信

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,15 +1,17 @@
 class BookCommentsController < ApplicationController
+
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to request.referer
+    @book = Book.find(params[:book_id])
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = @book.id
+    @comment.save
+      render 'book_comments/index'
   end
 
   def destroy
-    BookComment.find(params[:book_id]).destroy
-    redirect_to request.referer
+    @book = Book.find(params[:book_id])
+    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
+    render 'book_comments/index'
   end
 
   private
@@ -17,4 +19,5 @@ class BookCommentsController < ApplicationController
   def book_comment_params
     params.require(:book_comment).permit(:comment)
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 Rails.application.routes.draw do
-  get 'relationships/followings'
-  get 'relationships/followers'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  root :to =>"homes#top"
+  devise_for :users
+  root to: "homes#top"
   get "home/about"=>"homes#about"
 
-  devise_for :users
+
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resource :favorites, only: [:create, :destroy]
-    resource :book_comments, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
   end
   resources :users, only: [:index,:show,:edit,:update] do
     resource :relationships,  only: [:create, :destroy]
