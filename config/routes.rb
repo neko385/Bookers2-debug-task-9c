557899d5ffe1328e9005a1086675278d893e1f67
neko385 <102905708+neko385@users.noreply.github.com>
2022-06-08 13:15:44 +0000

    [Add]グループ作成機能

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
   end
-
+  resources :groups, only: [:index, :show, :new, :create, :edit, :update]
   get "search" => "searches#search"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
